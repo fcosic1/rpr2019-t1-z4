e7f427246b1c71ce832d1904e9d96192277feb9f
Faris Cosic <fcosic1@etf.unsa.ba>
2020-10-30 13:28:46 +0100

    Napravljena funkcija dodajArtikl u Supermarket

--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,17 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    private Artikl[] artikli = new Artikl[1000];
+    private int broj = 0;
+    public boolean dodajArtikl(Artikl a){
+        if(broj< 50) {
+            artikli[broj] = new Artikl(a.getNaziv(), a.getCijena(), a.getKod());
+            broj++;
+            return true;
+        }
+        return false;
+    }
+
+
+
 }
